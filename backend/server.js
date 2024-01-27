46f1ad1e295b3aad4b9164f4e96d510da5deb437
Aqeel <aqeelten@gmail.com>
2024-01-27 15:30:44 +0530

    b - User model, users route, utils, middleware

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import cors from "cors";
 import dotenv from "dotenv";
 import express from "express";
 import mongoose from "mongoose";
+import userRouter from "./routes/users.js";
 
 dotenv.config();
 
@@ -9,6 +10,8 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+app.use("/users", userRouter);
 
 app.get("/", (req, res) => {
     res.send("EvaluateAI API");
