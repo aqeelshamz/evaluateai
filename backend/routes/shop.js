db5016e1fa9cb0011c81b0f6f7b3aa9e729d40c7
Aqeel <aqeelten@gmail.com>
2024-02-10 22:33:28 +0530

    b - paypal integration

--- backend/routes/shop.js
+++ backend/routes/shop.js
@@ -4,14 +4,13 @@ import crypto from "crypto";
 import stripe from "stripe";
 import express from "express";
 import Razorpay from "razorpay";
-import Paypal from "@paypal/checkout-server-sdk";
 import ShopItem from "../models/ShopItem.js";
 import Order from "../models/Order.js";
 import Limits from "../models/Limits.js";
 import Purchase from "../models/Purchase.js";
 import { validate } from "../middlewares/validate.js";
 import PaymentMethod from "../models/PaymentMethod.js";
-import { currency, merchantAddress, merchantName, razorpayThemeColor } from "../utils/utils.js";
+import { currency, merchantAddress, merchantName, paypalCurrency, razorpayThemeColor } from "../utils/utils.js";
 import Invoice from "../models/Invoice.js";
 
 dotenv.config();
@@ -24,8 +23,9 @@ const instance = new Razorpay({
 const router = express.Router();
 const stripeObj = stripe(process.env.STRIPE_SECRET_KEY);
 
-let environment = new Paypal.core.SandboxEnvironment(process.env.PAYPAL_CLIENT_ID, process.env.PAYPAL_CLIENT_SECRET);
-let paypalClient = new Paypal.core.PayPalHttpClient(environment);
+//PAYPAL BASE URL
+const base = "https://api-m.sandbox.paypal.com"; //sandbox
+//const base = "https://api-m.paypal.com"; //live
 
 router.get("/", validate, async (req, res) => {
     const paymentMethod = await PaymentMethod.findOne();
@@ -132,6 +132,83 @@ router.post("/create-order-stripe", validate, async (req, res) => {
 
 });
 
+//PAYPAL ACCESS TOKEN
+async function generateAccessToken() {
+    const response = await fetch(base + "/v1/oauth2/token", {
+        method: "post",
+        body: "grant_type=client_credentials",
+        headers: {
+            Authorization:
+                "Basic " + Buffer.from(process.env.PAYPAL_CLIENT_ID + ":" + process.env.PAYPAL_CLIENT_SECRET).toString("base64"),
+        },
+    });
+    const data = await response.json();
+    return data.access_token;
+}
+
+//CREATE ORDER FUNCTION (PAYPAL)
+async function createOrder(price) {
+    const accessToken = await generateAccessToken();
+    const url = `${base}/v2/checkout/orders`;
+    const response = await fetch(url, {
+        method: "post",
+        headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${accessToken}`,
+        },
+        body: JSON.stringify({
+            intent: "CAPTURE",
+            purchase_units: [
+                {
+                    amount: {
+                        currency_code: paypalCurrency,
+                        value: price.toString(),
+                    },
+                },
+            ],
+        }),
+    });
+    const data = await response.json();
+    return data;
+}
+
+//CREATE ORDER (PAYPAL)
+router.post("/create-order-paypal", validate, async (req, res) => {
+    const schema = joi.object({
+        itemId: joi.string().required(),
+    });
+
+    try {
+        const data = await schema.validateAsync(req.body);
+        const item = await ShopItem.findById(data.itemId);
+
+        if (!item) return res.status(400).send("Invalid Item");
+
+        //IMPLEMENT currency conversion here: item.price * conversionRate
+        var conversionRate = 1;
+        var convertedPrice = item.price * conversionRate;
+
+        const order = await createOrder(convertedPrice);
+
+        await Order.findOneAndDelete({ userId: req.user._id });
+
+        const newOrder = new Order({
+            userId: req.user._id,
+            itemId: data.itemId,
+            orderId: order.id,
+            amount: item.price,
+            paymentMethod: "paypal",
+        });
+
+        await newOrder.save();
+
+        return res.send(order);
+    }
+    catch (err) {
+        return res.status(500).send(err);
+    }
+});
+
 //CREATE ORDER (RAZORPAY)
 router.post('/create-order-razorpay', validate, async (req, res) => {
     const schema = joi.object({
@@ -183,7 +260,6 @@ router.post('/create-order-razorpay', validate, async (req, res) => {
 
         return res.json(resData);
     } catch (err) {
-        console.log(err);
         return res.status(500).json({ error: 'Failed to create order' });
     }
 });
@@ -277,6 +353,71 @@ router.post('/verify-razorpay-payment', validate, async (req, res) => {
 
         return res.send(await newInvoice.save());
     } else {
+        return res.status(400).send('Payment verification failed');
+    }
+});
+
+async function capturePayment(orderId) {
+    const accessToken = await generateAccessToken();
+    const url = `${base}/v2/checkout/orders/${orderId}/capture`;
+    const response = await fetch(url, {
+        method: "post",
+        headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${accessToken}`,
+        },
+    });
+    const data = await response.json();
+    return data;
+}
+
+//COMPLETE PURCHASE (PAYPAL)
+router.post('/verify-paypal-payment', validate, async (req, res) => {
+    const { orderId } = req.body;
+
+    const order = await Order.findOne({ orderId: orderId });
+
+    if (!order) return res.status(400).send('Invalid Order');
+
+    const capture = await capturePayment(orderId);
+
+    if (capture.status === "COMPLETED") {
+
+        const newPurchase = new Purchase({
+            userId: req.user._id,
+            itemId: order.itemId,
+            transactionId: orderId,
+            paymentMethod: "paypal",
+            amount: order.amount,
+        });
+
+        const item = await ShopItem.findById(order.itemId);
+
+        await Limits.findOneAndUpdate({ userId: req.user._id }, { $inc: { evaluatorLimit: item.evaluatorLimit, evaluationLimit: item.evaluationLimit } });
+
+        await newPurchase.save();
+        await Order.findOneAndDelete({ orderId: orderId });
+
+        const newInvoice = new Invoice({
+            purchaseId: newPurchase._id,
+            userId: req.user._id,
+            date: newPurchase.createdAt.toLocaleString().split(",")[0],
+            item: item.title + " (" + item.evaluatorLimit + " Evaluators, " + item.evaluationLimit + " Evaluations)",
+            amount: newPurchase.amount,
+            paymentMethod: "PayPal",
+            to: {
+                name: req.user.name,
+                email: req.user.email,
+            },
+            from: {
+                name: merchantName,
+                email: merchantAddress,
+            }
+        });
+        
+        return res.send(await newInvoice.save());
+    }
+    else {
         return res.status(400).send('Payment verification failed');
     }
 });
