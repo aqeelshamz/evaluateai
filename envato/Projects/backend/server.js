a59449b9f3215b27adbc5e6a2201fb11d212f285
Aqeel <aqeelten@gmail.com>
2024-02-11 23:24:34 +0530

    envato: documentation

---
+++ envato/Projects/backend/server.js
@@ -0,0 +1,39 @@
+import cors from "cors";
+import dotenv from "dotenv";
+import express from "express";
+import mongoose from "mongoose";
+import userRouter from "./routes/users.js";
+import evaluateRouter from "./routes/evaluate.js";
+import shopRouter from "./routes/shop.js";
+import classRouter from "./routes/class.js";
+import adminRouter from "./routes/admin.js";
+
+dotenv.config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use("/users", userRouter);
+app.use("/evaluate", evaluateRouter);
+app.use("/shop", shopRouter);
+app.use("/class", classRouter);
+app.use("/admin", adminRouter);
+
+app.get("/", (req, res) => {
+    res.send("EvaluateAI API");
+});
+
+async function connectDB() {
+    await mongoose.connect(process.env.DB_URL);
+    console.log("Connected to MongoDB");
+}
+
+connectDB();
+
+const port = process.env.PORT || 8080;
+
+app.listen(8080, () => {
+    console.log(`Server at http://localhost:${port}`);
+});
---
+++ envato/Projects/frontend/src/utils/utils.ts
@@ -0,0 +1,8 @@
+const serverURL = "http://localhost:8080"
+const currencySymbol = "₹";
+const appName = "EvaluateAI";
+//UploadThing
+const maxFileCount = 10;
+const maxFileSize = "8MB"; // 8MB
+
+export { serverURL, currencySymbol, appName, maxFileCount, maxFileSize };
